8f6f4d8d8d14b93c731d0f908907eac41961b8d2
hsw1920 <hsw1920@gmail.com>
2023-10-14 15:06:57 +0900

    Main Storyboard 삭제

---
+++ SplitIt/SplitIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitIt
+//
+//  Created by 홍승완 on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
